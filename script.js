87102fd911f5cf988157a40ed2b88e6fb334632a
RITISH <145755891+RITISHM03@users.noreply.github.com>
2023-10-25 23:29:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Code to handle form submission (you can use AJAX to send form data to a server)
+    alert("Form submitted!");
+});
